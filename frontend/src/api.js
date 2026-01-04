31e7e936bf9245d7f88d1ac9ad9ca0dc1af064ba
Mahithapathuri <105708632+Mahithapathuri@users.noreply.github.com>
2026-01-04 22:33:42 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 const API_BASE =
   import.meta.env.MODE === "development"
     ? "http://localhost:3001"
-    : "https://customer-backend-abc123.onrender.com";
+    : " https://customer-management-app-qqyx.onrender.com";
 
 export default API_BASE;
